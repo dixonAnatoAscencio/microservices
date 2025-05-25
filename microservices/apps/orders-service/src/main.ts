1838151e620eed9af307c07f3da9b862c03c153c
DixonCi2 <dixon.anato@ci2.co>
2025-05-25 11:39:43 -0500

    init microservices

---
+++ microservices/apps/orders-service/src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  //la funcion microservice crea un microservicio, no debe usarse para una api 
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.TCP, //capa de transporte 
+    options: {
+      port: 4001, //puerto de la microservicio
+    }
+  });
+  await app.listen();
+}
+bootstrap();
